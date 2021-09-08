041262b1183f5dd4487b470bbebd3dc60ad1ef6f
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-08 17:26:05 +0500

    Add files via upload

---
+++ Assignment#1AboutPossibleERRORS.py
@@ -0,0 +1,25 @@
+Print("HELLO EVERYONE")
+# This shows SYNTAX_ERROR.
+# REASON: Because I capitalized the first alphabet of keyword " print ".
+print ("WELCOME")
+# This shows SYNTAX_ERROR.
+# REASON: Because I use 'SPACE' after the keyword " print " as we know spaces are not allowed or supported in python.
+print(HY)
+# This shows SYNTAX_ERROR.
+# REASON: Because I didn't use the "strings literals or quotation marks".
+print("WHAT THE HELL"!)
+# This shows SYNTAX_ERROR.
+# REASON: Because I use "EXCLAMATION MARK !" after string in the parentyhesis.
+print(can you"KEEP CALM")
+# This shows SYNTAX_ERROR.
+# REASON: Because I use some words before "strings". 
+print("KEEP CALM");
+# This shows SYNTAX_ERROR.
+# REASON: Because I use ';' at the end of the line.As we know semi-colon is not allowed or supported in python.
+print("")
+# This shows SYNTAX_ERROR.
+# REASON: Because I didn't put any message in the string that comes outr as a output.
+# REASON: Because  string is " EMPTY ".
+print('HELLO WORLD')
+# This shows SYNTAX_ERROR.
+# REASON: Because "QUOTATION MARKS" are missing
