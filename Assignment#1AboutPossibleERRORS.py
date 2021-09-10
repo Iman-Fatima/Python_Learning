b18f0b2efe44e4a41896416a909cb48480dade3b
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-10 19:48:24 +0500

    Add files via upload

--- Assignment#1AboutPossibleERRORS.py
+++ Assignment#1AboutPossibleERRORS.py
@@ -1,9 +1,9 @@
 Print("HELLO EVERYONE")
 # This shows SYNTAX_ERROR.
 # REASON: Because I capitalized the first alphabet of keyword " print ".
-print ("WELCOME")
-# This shows SYNTAX_ERROR.
-# REASON: Because I use 'SPACE' after the keyword " print " as we know spaces are not allowed or supported in python.
+ print ("WELCOME")
+# This shows INDENTATION_ERROR.
+# REASON: Because I use 'SPACE' before the keyword " print " as we know spaces are not allowed or supported in python.
 print(HY)
 # This shows SYNTAX_ERROR.
 # REASON: Because I didn't use the "strings literals or quotation marks".
@@ -13,13 +13,15 @@ print("WHAT THE HELL"!)
 print(can you"KEEP CALM")
 # This shows SYNTAX_ERROR.
 # REASON: Because I use some words before "strings". 
-print("KEEP CALM");
+print ('SAY NO TO DRUG'S')
 # This shows SYNTAX_ERROR.
-# REASON: Because I use ';' at the end of the line.As we know semi-colon is not allowed or supported in python.
-print("")
+# REASON: Because I use APOSTROPHE' in the end of string
+print(DJ")
 # This shows SYNTAX_ERROR.
-# REASON: Because I didn't put any message in the string that comes outr as a output.
-# REASON: Because  string is " EMPTY ".
+# REASON: Because I didn't put PROPER QUOTATION MARKS.
+print('BRAVO")
+# This shows SYNTAX_ERROR.
+# REASON: Because I use DIFFERENT QUOTATION MARKS.
 print['HELLO WORLD']
 # This shows TYPE_ERROR.
 # REASON: Because "SQUARE BRACKETS" are used.
