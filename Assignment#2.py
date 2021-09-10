22b77fa084f55d2a840a072f1f2a41f3d8c91dfe
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-10 22:04:58 +0500

    Add files via upload

---
+++ Assignment#2.py
@@ -0,0 +1,40 @@
+# Q1
+# You are required to print more than one statements in the same line using separate 
+# print() method for each statement.
+
+
+number1 = "Welcome to the new" 
+number2 = " World of leaners"
+number3 = " #PYTHON_LEARNERS"
+final = number1+number2+number3
+print(number1), print(number2), print(number3)
+
+
+
+
+
+# Q2 and Q3
+# Print your complete information stored in self-explanatory variables.
+#  e.g. My name is Arham Rumi. I am teaching Python to Batch - 01. etc.
+# Print Data-Type of all the variables used in your program. e.g. int etc.
+
+
+
+name = 'IMAN FATIMA'
+age = 21
+height = 5.3 
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+# Q4
+# Add three different integers and print the result.
+
+
+
+dominos_branches = 10
+cheezious_branches = 21
+almaidah_branches = 34
+total_branches = dominos_branches + cheezious_branches + almaidah_branches
+print(total_branches)
