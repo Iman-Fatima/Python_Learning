d6fdbceea5b2b921eb5bc4e56f904f20e7fc4c16
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-11 21:54:40 +0500

    Add files via upload

---
+++ Assignment#3fstring.py
@@ -0,0 +1,41 @@
+# Q1
+# Define in your First Name, Last Name and age in three different variables and print them by using f-string.
+
+
+
+first_name = 'IMAN' 
+last_name = ' FATIMA'
+age = 21
+print(f"I am {first_name + last_name} and I am {age}")
+
+
+
+
+# Q2
+# Print Data-Type of all the variables used in your program. e.g. int etc. by using f-string.
+
+
+
+first_name = 'IMAN' 
+last_name = ' FATIMA'
+age = 21
+print(f"My first name is  {first_name } which has the datatype of {type(first_name)} and my last name is {last_name} which has the datatype of {type(last_name)}")
+print(f"As you all know that my age is {age} its datatype is {type(age)}")
+
+
+
+# Q3
+# Print your complete information stored in self-explanatory variables. e.g. 
+# My name is Arham Rumi. I am 22 Years old by using f-string.
+
+
+
+first_name = 'IMAN' 
+last_name = ' FATIMA'
+age = 21
+course = " Python"
+cousre_instructor = "Mr. ARHAM RUMI "
+print(f"My  name is  {first_name + last_name}.")
+print(f"I am {age} years old.")
+print(f"I am learning {course} from a few days.")
+print(f"{cousre_instructor} is our couser instructor.")
