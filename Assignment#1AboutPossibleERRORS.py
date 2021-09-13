3a3183761cae0d2c01138fa0aa89d930ade1f61b
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-13 11:19:46 +0500

    Update Assignment#1AboutPossibleERRORS.py
    I commit changes in this file. I added output in this file.

--- Assignment#1AboutPossibleERRORS.py
+++ Assignment#1AboutPossibleERRORS.py
@@ -1,27 +1,38 @@
 Print("HELLO EVERYONE")
-# This shows SYNTAX_ERROR.
+# Output ---> NameError: name 'Print' is not defined
 # REASON: Because I capitalized the first alphabet of keyword " print ".
+
  print ("WELCOME")
-# This shows INDENTATION_ERROR.
-# REASON: Because I use 'SPACE' before the keyword " print " as we know spaces are not allowed or supported in python.
+# Output ---> IndentationError: unexpected indent
+# REASON: Because I use 'SPACE' before the keyword " print " as we know 
+# spaces are not allowed or supported in python before writing a piece of code.
+
 print(HY)
-# This shows SYNTAX_ERROR.
+# Output ---> NameError: name 'Print' is not defined
 # REASON: Because I didn't use the "strings literals or quotation marks".
+
 print("WHAT THE HELL"!)
-# This shows SYNTAX_ERROR.
+# Output ---> SyntaxError: invalid syntax
 # REASON: Because I use "EXCLAMATION MARK !" after string in the parentyhesis.
+
 print(can you"KEEP CALM")
-# This shows SYNTAX_ERROR.
+# Output ---> SyntaxError: invalid syntax
 # REASON: Because I use some words before "strings". 
+
 print ('SAY NO TO DRUG'S')
-# This shows SYNTAX_ERROR.
-# REASON: Because I use APOSTROPHE' in the end of string
+# Output ---> SyntaxError: invalid syntax
+# REASON: Because I use APOSTROPHE' in the end of string.
+
 print(DJ")
-# This shows SYNTAX_ERROR.
+# Output ---> SyntaxError: EOL while scanning string literal
 # REASON: Because I didn't put PROPER QUOTATION MARKS.
+      
 print('BRAVO")
-# This shows SYNTAX_ERROR.
+# Output ---> SyntaxError: EOL while scanning string literal
 # REASON: Because I use DIFFERENT QUOTATION MARKS.
+      
 print['HELLO WORLD']
-# This shows TYPE_ERROR.
+# Output ---> TypeError: 'builtin_function_or_method' object is not subscriptable
 # REASON: Because "SQUARE BRACKETS" are used.
+      
+      
