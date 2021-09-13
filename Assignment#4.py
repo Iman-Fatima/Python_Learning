8c6a273df350f0526cfe7f0576ffbb042ea1eb8f
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-13 19:18:49 +0500

    Update Assignment#4.py
    Added the function of  SUBTRACTION.

--- Assignment#4.py
+++ Assignment#4.py
@@ -13,3 +13,13 @@ number_2 = 10
 number_3 = number_1 + number_2
 print(f'The sum of the given numbers are: {number_3}')
 # The sum of the given numbers are: 15
+
+
+
+
+# --> SUBTRACTION:
+number_1 = 5
+number_2 = 10
+number_3 = number_1 - number_2
+print(f'The subtraction of the given numbers are: {number_3}')
+# The subtraction of the given numbers are: 15
