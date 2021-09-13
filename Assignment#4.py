115a8dd6883b982a034b30d9bef82c3bbd3bb2a7
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-13 19:45:13 +0500

    Update Assignment#4.py
    I commit a new operation in the above file.

--- Assignment#4.py
+++ Assignment#4.py
@@ -15,11 +15,8 @@ print(f'The sum of the given numbers are: {number_3}')
 # The sum of the given numbers are: 15
 
 
-
-
 # --> SUBTRACTION:
-number_1 = 5
-number_2 = 10
+
 number_3 = number_1 - number_2
 print(f'The subtraction of the given numbers are: {number_3}')
 # The subtraction of the given numbers are: 15
@@ -47,3 +44,10 @@ number_2 = 5
 number_3 = number_1 // number_2
 print(f'The floor division of the given numbers are: {number_3}')
 # The floor division of the given numbers are: 2
+
+# --> EXPONENTIATION:
+number_1 = 5
+number_2 = 4
+number_3 = number_1 ** number_2
+print(f'The exponentiation of the given numbers are: {number_3}')
+# The exponentiation of the given numbers are: 625
