3d1478d78788df00bbaaf369b81e74ee6082d562
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-13 19:08:39 +0500

    Add files via upload

---
+++ Assignment#4.py
@@ -0,0 +1,15 @@
+# Create a Mini Calculator that can operate on two operands and can perform:
+#     Addition
+#     Subtraction
+#     Multiplication
+#     Division
+#     Floor Division
+#     Exponentiation
+#     # Modulus
+
+# --> ADDITION:
+number_1 = 5
+number_2 = 10
+number_3 = number_1 + number_2
+print(f'The sum of the given numbers are: {number_3}')
+# The sum of the given numbers are: 15
