d9dae2a6cd191369716fbefd4e1eff1723ecac67
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-13 19:32:08 +0500

    Update Assignment#4.py
    I added more operations in my file.

--- Assignment#4.py
+++ Assignment#4.py
@@ -23,3 +23,27 @@ number_2 = 10
 number_3 = number_1 - number_2
 print(f'The subtraction of the given numbers are: {number_3}')
 # The subtraction of the given numbers are: 15
+
+
+
+# --> MULTIPLICATION:
+
+number_3 = number_1 * number_2
+print(f'The multiplication of the given numbers are: {number_3}')
+# The multiplication of the given numbers are: 50
+
+
+# --> DIVISION:
+number_1 = 10
+number_2 = 5
+number_3 = number_1 / number_2
+print(f'The division of the given numbers are: {number_3}')
+# The division of the given numbers are: 2.0
+
+
+# --> FLOOR DIVISION:
+number_1 = 10
+number_2 = 5
+number_3 = number_1 // number_2
+print(f'The floor division of the given numbers are: {number_3}')
+# The floor division of the given numbers are: 2
