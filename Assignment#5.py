a102ef44e6797c97985c08d3736c4402b064c442
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-15 15:39:07 +0500

    Add files via upload

---
+++ Assignment#5.py
@@ -0,0 +1,73 @@
+# TASK 1:-
+# Create a Mini Calculator that can operate on two operands and can perform:
+#     Addition
+#     Subtraction
+#     Multiplication
+#     Division
+#     Floor Division
+#     Exponentiation
+#     Modulus
+#     You Are Not Allowed To Use Third Variable For Storing Results
+#     Use Shorthand Method For Calculations 
+# TASK 2:-
+#     Operand_1 < Operand_2
+
+
+#  Operand_1 > Operand_2
+# --> ADDITION:
+
+number_1 = 6
+number_2 = 5
+print(f"The sum of two variables {number_1 + number_2}")
+# <--------------- OUTPUT --------------->  
+# The sum of the given numbers are: 11
+
+
+#  --> SUBTRACTION:
+number_1= number_1 - number_2
+print(f'The subtraction of the given numbers are: {number_1 }')
+# <--------------- OUTPUT --------------->  
+#  The subtraction of the given numbers are: 1
+
+
+#  --> MULTIPLICATION:
+
+number_1= number_1 * number_2
+print(f'The multiplication of the given numbers are: {number_1 }')
+# <--------------- OUTPUT --------------->  
+#  The multiplication of the given numbers are: 30
+
+
+#  --> DIVISION:
+number_1= number_1 / number_2
+print(f'The division of the given numbers are: {number_1 }')
+# <--------------- OUTPUT --------------->  
+#  The division of the given numbers are: 1.2
+
+
+#  --> FLOOR DIVISION:
+
+print(f'The floor division of the given numbers are: {number_1 // number_2}')
+# <--------------- OUTPUT --------------->  
+#  The floor division of the given numbers are: 1
+
+
+#  --> EXPONENTIATION:
+
+print(f'The exponentiation of the given numbers are: {number_1 ** number_2}')
+# <--------------- OUTPUT --------------->  
+#  The exponentiation of the given numbers are: 7776
+
+
+#  --> MODULUS:
+
+print(f'The modulus of the given numbers are: {number_1 % number_2}')
+# <--------------- OUTPUT --------------->  
+#  The modulus of the given numbers are: 1
+
+
+
+
+
+
+# ----------------------------------------------------------------------------------------------------------.
