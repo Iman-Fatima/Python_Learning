b08a4debf72cfbc811f72da311918daa959e514c
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-15 17:59:20 +0500

    Update Assignment#4.py

--- Assignment#4.py
+++ Assignment#4.py
@@ -1,3 +1,4 @@
+# TASK 1:-
 # Create a Mini Calculator that can operate on two operands and can perform:
 #     Addition
 #     Subtraction
@@ -7,11 +8,13 @@
 #     Exponentiation
 #     # Modulus
 
+# ---> Operand_1 > Operand_2
 # --> ADDITION:
-number_1 = 5
-number_2 = 10
+number_1 = 10
+number_2 = 5
 number_3 = number_1 + number_2
 print(f'The sum of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
 # The sum of the given numbers are: 15
 
 
@@ -19,14 +22,15 @@ print(f'The sum of the given numbers are: {number_3}')
 
 number_3 = number_1 - number_2
 print(f'The subtraction of the given numbers are: {number_3}')
-# The subtraction of the given numbers are: 15
-
+# <--------------- OUTPUT --------------->  
+# The subtraction of the given numbers are: 5
 
 
 # --> MULTIPLICATION:
 
 number_3 = number_1 * number_2
 print(f'The multiplication of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
 # The multiplication of the given numbers are: 50
 
 
@@ -35,6 +39,7 @@ number_1 = 10
 number_2 = 5
 number_3 = number_1 / number_2
 print(f'The division of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
 # The division of the given numbers are: 2.0
 
 
@@ -43,15 +48,17 @@ number_1 = 10
 number_2 = 5
 number_3 = number_1 // number_2
 print(f'The floor division of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
 # The floor division of the given numbers are: 2
+
 
 # --> EXPONENTIATION:
 number_1 = 5
 number_2 = 4
 number_3 = number_1 ** number_2
 print(f'The exponentiation of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
 # The exponentiation of the given numbers are: 625
-
 
 
 # --> MODULUS:
@@ -59,7 +66,72 @@ number_1 = 6
 number_2 = 4
 number_3 = number_1 % number_2
 print(f'The modulus of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
 # The modulus of the given numbers are: 2
 
 
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#     Operand_1 < Operand_2
+
+
+number_1 = 5
+number_2 = 10
+number_3 = number_1 + number_2
+print(f'The sum of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The sum of the given numbers are: 15
+
+
+# --> SUBTRACTION:
+
+number_3 = number_1 - number_2
+print(f'The subtraction of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The subtraction of the given numbers are: -5
+
+
+# --> MULTIPLICATION:
+
+number_3 = number_1 * number_2
+print(f'The multiplication of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The multiplication of the given numbers are: 50
+
+
+# --> DIVISION:
+number_1 = 5
+number_2 = 10
+number_3 = number_1 / number_2
+print(f'The division of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The division of the given numbers are: 0.5
+
+
+# --> FLOOR DIVISION:
+number_1 = 5
+number_2 = 10
+number_3 = number_1 // number_2
+print(f'The floor division of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The floor division of the given numbers are: 0
+
+
+# --> EXPONENTIATION:
+number_1 = 5
+number_2 = 6
+number_3 = number_1 ** number_2
+print(f'The exponentiation of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The exponentiation of the given numbers are: 15625
+
+
+# --> MODULUS:
+number_1 = 4
+number_2 = 6
+number_3 = number_1 % number_2
+print(f'The modulus of the given numbers are: {number_3}')
+# <--------------- OUTPUT --------------->  
+# The modulus of the given numbers are: 4
+
+
+# ----------------------------------------------------------------------------------------------------------.
