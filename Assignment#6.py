28b4fae601999dc38fbeb5dc0419d8069dd8efef
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-16 00:21:33 +0500

    Add files via upload

---
+++ Assignment#6.py
@@ -0,0 +1,20 @@
+#  comparison operators
+# -------------------------------------------------------------------------------------------------------
+#     Use different Comparison Operators
+#         Equal to
+#         Greater than or equal to
+#         Not equal to
+#         Less than or equal to
+#         Greater than
+#         Less than
+# Note
+#     Output result of all of the above operations using f-string with proper string message. 
+#     e.g. Result of Operand_1 == Operand_2 = calulated_result
+#     Comment your outputs in the program
+# ---------------------------------------------------------------------------------------------------------
+
+# ---> Equal to:-
+operand_1 = 5
+operand_2 = 5
+result = (operand_1 == operand_2)
+print(f"The given operands are equal (==) to each other : {result}")
