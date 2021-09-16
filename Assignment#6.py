22cce7079684d1da7239fbd1dc66df3fb33a467a
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-16 21:43:26 +0500

    Update Assignment#6.py

--- Assignment#6.py
+++ Assignment#6.py
@@ -28,9 +28,11 @@ print(f" The given statement is: {result}")
 operand_1 = 4
 operand_2 = 5
 result = (operand_1 >= operand_2)
-print(f"The given operands {operand_1} and {operand_2} are greater than or equal (>=) to each other : {result}")
+print(f"The operand {operand_1} is greater than or equal (>=) to operand {operand_2}.")
+print(f" The given statement is: {result}")
 # <--------------- OUTPUT --------------->  
-# The given operands 4 and 5 are greater than or equal (>=) to each other : False
+# The operand 4 is greater than or equal (>=) to operand 5.
+#  The given statement is: False
 
 
 # ---> Not equal to:-
