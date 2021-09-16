2fd25f82623a631179b701a5f86ea7193c429a93
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-16 21:45:59 +0500

    Update Assignment#6.py

--- Assignment#6.py
+++ Assignment#6.py
@@ -44,14 +44,15 @@ print(f"The given operands {operand_1} and {operand_2} are Not equal (!=) to eac
 #The given operands 4 and 5 are Not equal (!=) to each other : True
 
 
-# ---> Less than or equal to:-
+# ---> Not equal to:-
 operand_1 = 4
 operand_2 = 5
-result = (operand_1 <= operand_2)
-print(f"The given operands {operand_1} and {operand_2} are Less than or equal (<=) to each other : {result}")
+result = (operand_1 != operand_2)
+print(f"The operand {operand_1} is Not equal (!=) to operand {operand_2}.")
+print(f" The given statement is: {result}")
 # <--------------- OUTPUT --------------->  
-#The given operands 4 and 5 are Less than or equal (<=) to each other : True
-
+# The operand 4 is Not equal (!=) to operand 5.
+#  The given statement is: True
 
 # ---> Greater than to:
 operand_1 = 4
