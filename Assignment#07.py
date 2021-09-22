2affc18e5d0e1ba72e361b26c47c2ef83eaf7406
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-22 23:57:58 +0500

    Update Assignment#07.py

--- Assignment#07.py
+++ Assignment#07.py
@@ -2,9 +2,8 @@
 # Note
 # Output result of all of the above operations using f-string with proper string message. e.g. Result of statement = calulated_result
 # Perform Dry Run
-    For example
-
-    res = 7 + 9 / 3 - 4 * 2
+#     For example
+#     res = 7 + 9 / 3 - 4 * 2
 
     7 + 3 - 4 * 2
 
