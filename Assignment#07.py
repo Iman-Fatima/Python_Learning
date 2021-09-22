41a9132a933174c0711b4a9782acd59657caf913
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-22 23:55:49 +0500

    Update Assignment#07.py

--- Assignment#07.py
+++ Assignment#07.py
@@ -1,3 +1,3 @@
 # Use all the Algebraic Operators in accordance with the Precedence
 # Note
-    # Output result of all of the above operations using f-string with proper string message. e.g. Result of statement = calulated_result
+# Output result of all of the above operations using f-string with proper string message. e.g. Result of statement = calulated_result
