b0e7fa659150695de8a7b86feabb3c47ae812edc
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-25 21:26:41 +0500

    Update Assignment#08.py

--- Assignment#08.py
+++ Assignment#08.py
@@ -11,3 +11,9 @@ if shopping:
 # Problem 02:-
 # Suppose that you are a teacher, and you have make a list of all the students 
 # who passed the examination successfully. Suppose that the passing criteria is 60% marks.
+
+marks = 100
+# If students fulfill the criteria of 60% marks then they'll pass.
+if marks >= 60:
+    print("Congratulations! You have passed the examination, successfully.")
+# Congratulations! You have passed the examination, successfully.
