4e06da6b433754204bfdbeb0589b00261b5c5cca
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-25 21:25:43 +0500

    Update Assignment#08.py

--- Assignment#08.py
+++ Assignment#08.py
@@ -7,3 +7,7 @@ shopping = True
 if shopping:
     print("Have a great day")
 # Have a great day
+
+# Problem 02:-
+# Suppose that you are a teacher, and you have make a list of all the students 
+# who passed the examination successfully. Suppose that the passing criteria is 60% marks.
