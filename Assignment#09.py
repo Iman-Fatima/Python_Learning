48cbaa90e4028ab89c367a3b20d676edb301dec1
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-28 21:59:30 +0500

    Update Assignment#09.py

--- Assignment#09.py
+++ Assignment#09.py
@@ -1,2 +1,10 @@
 # Problem # 1
 # Create a program that will allow people over 18 and below 30 to watch IRON MAN 3
+
+age = 20
+ 
+if age >18 and age <30:
+    print(f"Hurrah, you are allowed to watch IRON MAN 3.")
+
+else :
+    print(f"Awww Sorry, you are not llowed to watch IRON MAN 3.")
