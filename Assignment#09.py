141c31c748bd130520ab54b22912d2223d87d7c0
Iman Fatima <90279810+Iman-Fatima@users.noreply.github.com>
2021-09-28 22:23:20 +0500

    Update Assignment#09.py

--- Assignment#09.py
+++ Assignment#09.py
@@ -18,3 +18,6 @@ else :
 # and this program fullfill the desired condition and showing hurra message for those who 
 # can watch the movie and show Awww Sorry message for those who can't watch the movie.
 
+# PROBLEM 02:-
+# Create a program that will allow people over 4 and below 25 or above 50 and below 60 to watch cartoons
+age = 35
